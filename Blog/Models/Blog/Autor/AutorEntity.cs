b91c0badedfae529c32f87ced9b63cb4cdfd6f67
Iohana <iohana.naila@hotmail.com>
2020-02-14 21:35:01 -0300

    meu primeiro commit na aula yay

---
+++ Blog/Models/Blog/Autor/AutorEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Autor
+{
+    public class AutorEntity
+    {
+        private string nome;
+
+        public string Nome { get => nome; set => nome = value; }
+    }
+}
---
+++ Blog/Models/Blog/Postagem/PostagemEntity.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Models.Blog.Autor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        private string titulo;
+        private AutorEntity autor;
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public AutorEntity Autor { get => autor; set => autor = value; }
+
+        public virtual string editar()
+        {
+            return "edição realizada";
+        }
+
+        public class PostagemSobreFilmeEntity : PostagemEntity
+        {
+            public string Genero { get; set; }
+
+            public override string editar()
+            {
+                return "edição realizada na postagem sobre filmes";
+            }
+        }
+
+    }
+}
